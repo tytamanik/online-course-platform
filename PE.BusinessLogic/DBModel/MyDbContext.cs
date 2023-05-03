68597de0eabcb308e6c1bd3b5c59c95f72fc4911
IonCaus <ion.caus@isa.utm.md>
2023-05-03 08:52:56 +0300

    Business Logic, Domain, Helper layers

---
+++ PE.BusinessLogic/DBModel/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using PE.Domain.Entities.User;
+
+namespace PE.BusinessLogic.DBModel
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ PE.web/Controllers/ULoginController.cs
@@ -0,0 +1,48 @@
+﻿using PE.BusinessLogic.DBModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using PE.web.Models;
+
+namespace PE.web.Controllers
+{
+    public class ULoginController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        // GET: 
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        void connectionString()
+        {
+            con.ConnectionString = "data source=WIN-QJ63609DU36; database=PlatEd; integrated security = SSPI "
+        }
+
+        public ActionResult Verify(UserLogin log)
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from";
+            dr = com.ExecuteReader();
+            if(dr.Read())
+            {
+                return View();
+            }
+            else 
+            {
+                return View();
+            }
+            con.Close();    
+            return View();
+        }
+    }
+}
